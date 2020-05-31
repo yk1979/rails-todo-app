a923a6204247f3553371967ebee186756f04c7b8
yk1979 <yukiirisawa1979@gmail.com>
2020-06-01 02:03:48 +0900

    implement index, new, create controller

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,13 +1,27 @@
 class TasksController < ApplicationController
   def index
+    @tasks = Task.all
   end
 
   def show
   end
 
   def new
+    @task = Task.new
   end
 
   def edit
+  end
+
+  def create
+    task = Task.new(task_params)
+    task.save!
+    redirect_to tasks_url, notice: "successfully added new task: #{task.name}"
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:name, :description)
   end
 end
