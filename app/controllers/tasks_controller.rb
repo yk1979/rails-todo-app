9f649be6d0eaa09dfa5782a4dbc565ac063aec91
yk1979 <yukiirisawa1979@gmail.com>
2020-06-01 03:13:20 +0900

    implement destroy function

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -27,6 +27,12 @@ class TasksController < ApplicationController
     redirect_to tasks_url, notice: "successfully updated the task: #{task.name}"
   end
 
+  def destroy
+    task = Task.find(params[:id])
+    task.destroy
+    redirect_to tasks_url, notice: "successfully deleted the task: #{task.name}"
+  end
+
   private
 
   def task_params
